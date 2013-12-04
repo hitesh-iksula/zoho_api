b8defb26928e08833538b61bf7682b4d846dc174
Hitesh <hitesh.pachpor@iksula.com>
2013-12-05 01:09:55 +0530

    Show time in hours:minutes format, remove commented code

--- index.php
+++ index.php
@@ -32,9 +32,6 @@ foreach ($projectsData['result']['ProjectDetails'] as $index => $object) {
 	$helper = new Formatting_Helper();
 	$project = $helper->getArrayFromXmlObject($object->ProjectDetail);
 
-	// echo "<h2>" . $project['project_name'] . "</h2>";
-	// print_r($project);
-
 	$projectId = $project['project_id'];
 	$params = array(
 		'projId' => $projectId,
@@ -52,7 +49,6 @@ foreach ($projectsData['result']['ProjectDetails'] as $index => $object) {
 
 	$logsData = $logsObject->getFormattedResponse();
 
-	// print_r($logsData);
 	if($logsData != 'No tasks found') {
 		$project['tasks'] = current($logsData);
 		$projects[] = $project;
@@ -64,10 +60,12 @@ foreach ($projectsData['result']['ProjectDetails'] as $index => $object) {
 
 <!doctype html>
 <html>
+
 	<head>
 		<link rel="stylesheet" href="style.css"/>
 		<link href='http://fonts.googleapis.com/css?family=Raleway:400,300,600' rel='stylesheet' type='text/css'>
 	</head>
+
 	<body>
 
 		<div class="projects">
@@ -77,16 +75,16 @@ foreach ($projectsData['result']['ProjectDetails'] as $index => $object) {
 				<div class="project">
 					
 					<div class="pad_left project_name">
-						<h1><?php echo $project['project_name'] ?></h1>
+						<h1><?php echo $project['project_name']; ?></h1>
 					</div>
 					<div class="pad_left project_member">
-						<h2><?php echo 'Hitesh Pachpor' ?></h2>
+						<h2><?php echo 'Hitesh Pachpor'; ?></h2>
 					</div>
 
 					<?php $tasks = $project['tasks']; ?>
 					<div class="pad_left date_and_hours">
-						<div class="date">Date: <span><?php echo $tasks['date'] ?></span></div>
-						<div class="total_hrs">Total Hours of Work Done: <span><?php echo $tasks['total_loghours_perday'] ?></span></div>
+						<div class="date">Date: <span><?php echo $tasks['date']; ?></span></div>
+						<div class="total_hrs">Total Hours of Work Done: <span><?php echo str_replace(".", ":", $tasks['total_loghours_perday']); ?></span></div>
 					</div>
 
 					<div class="task_list">
@@ -97,8 +95,8 @@ foreach ($projectsData['result']['ProjectDetails'] as $index => $object) {
 						<?php foreach($tasks['tasks'] as $task): ?>
 							<div class="task_row">
 								<div class="arrow_right"></div>
-								<div class="task"><?php echo $task['task'] ?></div>
-								<div class="loghours"><?php echo $task['loghours'] ?></div>
+								<div class="task"><?php echo $task['task']; ?></div>
+								<div class="loghours"><?php echo str_replace(".", ":", $task['loghours']); ?></div>
 							</div>
 						<?php endforeach; ?>
 					</div>
@@ -110,4 +108,5 @@ foreach ($projectsData['result']['ProjectDetails'] as $index => $object) {
 		</div>
 
 	</body>
+
 </html>
